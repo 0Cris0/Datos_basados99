30b283f488c94a5d36214c0a88b05fa2a6f80fa1
Cristóbal Albornoz <cristbal.albornoz@uc.cl>
2023-11-23 20:47:05 -0300

    Actualización query (2/4)

--- Misc/querys_teoricas.sql
+++ Misc/querys_teoricas.sql
@@ -19,7 +19,7 @@ repeticion_usuario($n_usuario)
 $n_usuario -- nombre del usuario a consultar 
 
 $contrasena -- contrasena ingresada 
-CREATE OR REPLACE FUNCTION credenciales_usuario (n_usuario varchar, contrasena) RETURNS boolean AS $$ 
+CREATE OR REPLACE FUNCTION credenciales_usuario (n_usuario varchar, contrasena varchar) RETURNS boolean AS $$ 
 BEGIN
     RETURN EXISTS (
         SELECT 1
@@ -34,14 +34,19 @@ credenciales_usuario($n_usuario, $contrasena)
 -- =================================
 -- agregación de un nuevo usuario
 
--- [AGREGAR LOS PARAMETROS DEL USUARIO]
-CREATE OR REPLACE Function agregar_usuario (***parametro tipo, …**) RETURNS void AS $$
+$nombre 
+$mail 
+$contrasena 
+$username
+$fecha_nacimiento
+
+CREATE OR REPLACE Function agregar_usuario (nombre varchar, mail varchar, contrasena varchar, username varchar, fecha_nacimiento date) RETURNS void AS $$
 BEGIN
-    INSERT INTO usuarios VALUES (**nombre de los parametros**)
+    INSERT INTO usuarios VALUES (nombre, mail, contrasena, username, fecha_nacimiento)
 END
 $$ language plpgsql 
 
-agregar_usuario()
+agregar_usuario($nombre, $mail, $contrasena, $username, $fecha_nacimiento)
 
 -- =================================
 -- obtener información personal del usuario
@@ -57,33 +62,37 @@ CREATE MATERIALIZED VIEW info_perfil AS
     WHERE $n_usuario == u.nombre 
 
     -- Parte 2: Listado de las subscripciones a juegos
-    SELECT * 
-    FROM subscripciones_juego as sj 
+    SELECT sj.id, vs.titulo
+    FROM subscripciones_juego as sj, videojuegos_sub as vs
     WHERE $id_usuario == sj.id_usuario 
+    AND vs.id_juego == sj.id_juego
 
     -- Parte 3: Listado de subscripciones streaming
-    SELECT * 
-    FROM subscripciones_pelis as sp 
+    SELECT sp.id_sub, pp.nombre
+    FROM subscripciones_ps as sp, proveedores_ps as pp
     WHERE $id_usuario == sp.id_usuario 
+    AND sp.id_prov == pp.id_prov
 
     --Parte 4: Tiempo visto en streaming
     SELECT SUM(streaming.duracion)
     FROM (
         -- Parte 4.1: Tiempo visto en peliculas
         SELECT SUM(p.duracion) as duracion
-        FROM visualizaciones_pelis as vp, peliculas as p 
+        FROM visualizaciones_p as vp, peliculas as p 
         WHERE $id_usuario == vp.id_usuario 
+        AND p.id_pelicula == vp.id_pelicula
         
         UNION
         -- Parte 4.2: Tiempo visto en series
         SELECT SUM(c.duracion) as duracion
-        FROM visualizaciones_capitulos as vc, capitulo as c  
+        FROM visualizaciones_c as vc, capitulo as c  
         WHERE $id_usuario == vc.id_usuario
+        AND c.id_capitulo == vc.id_capitulo
     ) AS streaming
 
     -- Parte 5: Tiempo visto en juegos
     SELECT SUM(vu.duracion) as duracion
-    FROM videojuegos_usuario as vu 
+    FROM videojuegos_usuario as vu
     WHERE $id_usuario == vu.id_usuario 
 
     -- Parte 6: Edad
@@ -115,12 +124,10 @@ CREATE MATERIALIZED VIEW info_perfil AS
 
 
 
-
-
 -- =================================
 -- obtener proveedores de streaming
 SELECT * 
-FROM  proveedores_peli 
+FROM  proveedores_ps 
 
 -- =================================
 -- detalle del proveedor (valor sub, cantidad pelis y series)
@@ -140,23 +147,23 @@ BEGIN
         FROM 
             (
                 SELECT pp.costo as monto
-                FROM  proveedores_peli as pp
-                WHERE pp.id_prove == id_proveedor) as m,
+                FROM  proveedores_ps as pp
+                WHERE pp.id_prov == id_proveedor) as m,
             (
                 SELECT COUNT(p.titulo) as cantidad_pelis
-                FROM pelis_proveedor_peli as ppp, peliculas as p 
-                WHERE ppp.id_prove == id_proveedor) as p,
+                FROM catalogo_p_incluida as cpi
+                WHERE cpi.id_prov == id_proveedor) as p,
             (
                 SELECT COUNT(s.serie) as cantidad_series
-                FROM series_proveedor_peli as ssp, series as s 
-                WHERE ssp.id_prove == id_proveedor) as s
+                FROM catalogo_s as cs
+                WHERE cs.id_prove == id_proveedor) as s
         ';
     RETURN;
 END
 $$ language plpgsql
 
 detalle_proveedor($id_proveedor)
-            
+
 -- =================================
 -- top 3 series del proveedor
 
@@ -167,11 +174,12 @@ FROM (
     SELECT s.serie, s.puntuacion, vs.n_visualizaciones
     FROM series as s, (
         SELECT c.id_serie, COUNT(*) as n_visualizaciones
-        FROM visualizaciones_capitulos as vc, series_proveedor_peli as ssp, capitulo as c
-        WHERE ssp.id_prove == $id_proveedor
+        FROM visualizacion_c as vc, catalogo_s as cs, capitulo as c
+        WHERE cs.id_prove == $id_proveedor
+        AND c.id_capitulo == vc.id_capitulo
         GROUP BY c.id_serie
         ) as vs
-    WHERE s.id == vs.id_serie
+    WHERE s.id_serie == vs.id_serie
     ORDER BY vs.n_visualizaciones) as datos
 
 -- =================================
@@ -183,11 +191,12 @@ SELECT TOP 3 *
 FROM (
     SELECT p.titulo, p.puntuacion, vp.n_visualizaciones
     FROM peliculas as p, (
-        SELECT  (c.id_peli, COUNT(*) as n_visualizaciones)
-        FROM visualizaciones_peli as vp, pelis_proveedor_peli as ppp
-        WHERE ppp.id_prove == $id_proveedor
+        SELECT  (cpi.id_pelicula, COUNT(*) as n_visualizaciones)
+        FROM visualizacion_p as vp, catalogo_p_incluida as cpi
+        WHERE cpi.id_prove == $id_proveedor
+        AND vp.id_pelicula == cpi.id_pelicula
         ) as vp
-    WHERE p.id_peli == vp.id_peli
+    WHERE p.id_pelicula == vp.id_pelicula
     ORDER BY vp.n_visualizaciones) as datos
 
 -- =================================
@@ -200,22 +209,22 @@ $n_contenido
 -- Caso serie 
 SELECT EXISTS( 
 	SELECT 1 
-    FROM series_proveedor_peli as spp, serie as s, proveedores_peli as pp 
+    FROM catalogo_s as cs, serie as s, proveedores_ps as pp 
     WHERE pp.nombre == $n_proveedor 
+    AND cs.id_serie == s.id_serie
+    AND cs.id_prov == pp.id_prov
     AND s.serie == $n_contenido 
-    AND spp.id_prov == pp.id 
-    AND s.id_serie == spp.id_serie 
 ) 
 
 -- Caso película 
 
 SELECT EXISTS( 
 	SELECT 1 
-    FROM pelis_proveedor_peli as ppp, pelicula as p, proveedores_peli as pp 
+    FROM catalogo_p_incluida as cpi, pelicula as p, proveedores_ps as pp 
     WHERE pp.nombre == $n_proveedor 
+    AND cpi.id_pelicula == p.id_pelicula
+    AND cpi.id_prov == pp.id_prov
     AND p.titulo == $n_contenido 
-    AND ppp.id_prov == pp.id 
-    AND p.id_peli == ppp.id_peli 
 ) 
 
 -- Hacerlo case insensitive
@@ -233,7 +242,7 @@ SELECT EXISTS(
 
 
 
-
+-- LISTO HACIA ARRIBA (hace sentido) ------------------------------------------------------------------------------------------------------------
 
 
 
